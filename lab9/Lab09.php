9115f727b3dbb312b00a342b73f19466c31bf806
FDUpiggy <piggy_zbw@pm.me>
2019-05-23 18:12:04 +0800

    lab9 finished

--- lab9/Lab09.php
+++ lab9/Lab09.php
@@ -42,8 +42,10 @@ include 'travel-data.inc.php';
               /* hint: use array and loop */
               //the next line is an example
               //<a href="list.php?country=Canada" role="button" class="btn btn-default"> Canada</a> 
-
-
+              sort($countries);
+              foreach ($countries as $country) {
+                echo '<a href="list.php?country="'.$country.'" role="button" class="btn btn-default">'.$country.'</a>';
+              }
               ?>
                      
         </div>               
@@ -70,7 +72,22 @@ include 'travel-data.inc.php';
    //              		</div>
    //              	</div>
    //              </a>
-			// </li>
+            // </li>
+            
+            foreach ($images as $image){
+              echo '<li>';
+              echo '<a href="detail.php?id='.$iamge[id].'" class="img=responsive">';
+              echo '<img src="images/square/'.$image[path].'" alt="'.$image[title].'">';
+              echo '<div class="caption">';
+              echo '<div class="blur">';
+              echo '</div>';
+              echo '<div class="caption-text">';
+              echo '<h1>'.$image[title].'</h1>';
+              echo '</div>';
+              echo '</div>';
+              echo '</a>';
+              echo '</li>';
+            }
           ?>
 
        </ul>       
